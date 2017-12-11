5b909b58d6004e3374c0241d81806635770cdaa3
Michal <michal@dorfin.waw.pl>
2017-12-11 12:15:59 +0100

    assddsfs

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -108,3 +108,37 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        request(app).delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toEqual(null);
+                    //expect(todo).toNotExist();  to niedziała?!
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+        request(app).delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+    it('should return 404 if todo for wrong id', (done) => {
+        var id = new ObjectID();
+        request(app).delete(`/todos/123`)
+            .expect(404)
+            .end(done);
+    });
+});
